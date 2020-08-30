03c478e1f5abe785d6f94e244fd03b435afd67e1
pg300 <pg300@DESKTOP-CQV2C1L>
2020-08-30 17:18:30 -0600

    adding  class

--- src/sisfacturacion/Factura.java
+++ src/sisfacturacion/Factura.java
@@ -9,6 +9,31 @@ package sisfacturacion;
  *
  * @author pg300
  */
-public class Factura {
+public abstract class Factura {
+    private int Codigo;
+    private double total;
+    private int cantidad;
+    private Cliente cliente1;
+    private String formaDePago;
+    private Fecha fechaVencimiento;
+    private Fecha diaActual;
+    private String ClaveElectronica;
+    private String NumeroFacturaElectronica;
+    private int Plazo;
+    public String getCodigo(){
+    return "FA-"+Codigo;} 
+    
+    public abstract void  crearFactura();
+
+    public Factura( int Codigo, double total, int cantidad, Cliente cliente1, String formaDePago, Fecha diaActual, String ClaveElectronica, String NumeroFacturaElectronica) {
+        this.Codigo =Codigo;
+        this.total = total;
+        this.cantidad = cantidad;
+        this.cliente1 = cliente1;
+        this.formaDePago = formaDePago;
+        this.diaActual = diaActual;
+        this.ClaveElectronica = ClaveElectronica;
+        this.NumeroFacturaElectronica = NumeroFacturaElectronica;
+    }
     
 }
