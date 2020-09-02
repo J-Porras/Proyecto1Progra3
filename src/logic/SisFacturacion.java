6e6b5325e2ce6183fbf0a6fbe27c7018c3a2ee33
Porras <japorras7@hotmail.com>
2020-09-02 17:51:21 -0600

    Merged master

--- src/logic/SisFacturacion.java
+++ src/logic/SisFacturacion.java
@@ -5,9 +5,15 @@
  */
 package logic;
 
-import Presentation.WindowProductos.Controller;
-import Presentation.WindowProductos.Model;
-import Presentation.WindowProductos.View;
+//import Presentation.WindowProductos.Controller;
+//import Presentation.WindowProductos.Model;
+//import Presentation.WindowProductos.View;
+
+import Presentation.WindowEmpresa.ControllerEmpresa;
+import Presentation.WindowEmpresa.ModelEmpresa;
+import Presentation.WindowEmpresa.ViewEmpresa;
+ 
+import Presentation.WindowCliente.View;
 
 /**
  *
@@ -18,12 +24,24 @@ public class SisFacturacion {
     /**
      * @param args the command line arguments
      */
+    
     public static void main(String[] args) {
       
-         Model model = new Model();
+        /*
+        Model model = new Model();
         View view = new View();
         Controller controller= new Controller(model,view) ;
+        view.setVisible(true);*/
+        
+        
+        
+        ViewEmpresa view = new ViewEmpresa();
         view.setVisible(true);
+
+        
+        
+        View view1 = new View();
+        view1.setVisible(true);
     }
     
 }
